d8759728bf99715e9154c57d635ba2b153d3001d
Bianca Correa <me@aquabianca.dev>
2026-01-07 19:05:25 -0300

    feat: decouple ssh connector into strategies

--- connector.go
+++ connector.go
@@ -8,22 +8,104 @@ import (
 	"golang.org/x/crypto/ssh"
 )
 
-type SshConnector struct {
-	User        string
-	Ip          string
-	Port        string
-	KeyFile     string
-	UseTerminal bool
+type Connector interface {
+	Connect() error
+	Close() error
+	Wait() error
 }
 
+type NativeSshProvider struct {
+	User    string
+	Ip      string
+	Port    string
+	KeyFile string
+
+	client *ssh.Client
+}
+
+// Connect establishes an SSH connection using the native Golang SSH package (golang.org/x/crypto/ssh).
+func (nc *NativeSshProvider) Connect() error {
+	cfg := easyssh.MakeConfig{
+		User:    nc.User,
+		Server:  nc.Ip,
+		Port:    nc.Port,
+		KeyPath: nc.KeyFile,
+	}
+
+	s, client, err := cfg.Connect()
+	if err != nil {
+		return err
+	}
+	defer s.Close()
+
+	nc.client = client
+
+	return nil
+}
+
+func (nc *NativeSshProvider) Close() error {
+	return nc.client.Close()
+}
+
+func (nc *NativeSshProvider) Wait() error {
+	return nc.client.Wait()
+}
+
+type BinSshProvider struct {
+	// Arguments for the SSH command.
+	args []string
+
+	// Command that manages the underlying ssh process.
+	cmd *exec.Cmd
+}
+
+func NewBinProvider(args ...string) Connector {
+	return &BinSshProvider{
+		args: args,
+	}
+}
+
+// Connect establishes a SSH connection using a local OpenSSH binary.
+func (bc *BinSshProvider) Connect() error {
+	sshCmd := exec.Command("ssh", bc.args...)
+
+	sshCmd.Stdin = os.Stdin
+	sshCmd.Stdout = os.Stdout
+	sshCmd.Stderr = os.Stderr
+
+	bc.cmd = sshCmd
+
+	if err := sshCmd.Start(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (bc *BinSshProvider) Close() error {
+	return bc.cmd.Process.Kill()
+}
+
+func (bc *BinSshProvider) Wait() error {
+	return bc.cmd.Process.Kill()
+}
+
+// Deprecated: use NativeProvider instead.
+type SshConnector struct {
+	User    string
+	Ip      string
+	Port    string
+	KeyFile string
+}
+
+// Deprecated: use NativeProvider instead.
 // Connect establishes an SSH connection to the specified user and IP address.
 func (c *SshConnector) Connect() (*ssh.Client, error) {
 	cfg := easyssh.MakeConfig{
-		User:       c.User,
-		Server:     c.Ip,
-		Port:       c.Port,
-		KeyPath:    c.KeyFile,
-		RequestPty: c.UseTerminal,
+		User:    c.User,
+		Server:  c.Ip,
+		Port:    c.Port,
+		KeyPath: c.KeyFile,
 	}
 
 	s, conn, err := cfg.Connect()
@@ -33,23 +115,4 @@ func (c *SshConnector) Connect() (*ssh.Client, error) {
 	defer s.Close()
 
 	return conn, nil
-}
-
-// Interactive starts a remote interactive shell using OpenSSH's client.
-func (c *SshConnector) Interactive(args ...string) error {
-	sshCmd := exec.Command("ssh", args...)
-
-	sshCmd.Stdin = os.Stdin
-	sshCmd.Stdout = os.Stdout
-	sshCmd.Stderr = os.Stderr
-
-	if err := sshCmd.Start(); err != nil {
-		return err
-	}
-
-	if err := sshCmd.Wait(); err != nil {
-		return err
-	}
-
-	return nil
 }
