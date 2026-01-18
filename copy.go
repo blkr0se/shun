155d8c1845f2238bbaac4f82094b96cf0e087db3
Bianca Correa <me@aquabianca.dev>
2026-01-18 17:59:58 -0300

    feat(copy): split chunked copy fn into smaller methods

--- copy.go
+++ copy.go
@@ -6,7 +6,6 @@ import (
 	"os"
 	"time"
 
-	"github.com/pkg/sftp"
 	"github.com/schollz/progressbar/v3"
 )
 
@@ -28,12 +27,13 @@ func (ao *AtOnce) Copy(dst io.Writer, src io.Reader) error {
 }
 
 type Chunked struct {
-	Source      *os.File
-	Destination *sftp.File
+	written int64
+
+	tracker *progressbar.ProgressBar
 }
 
-func (c *Chunked) defaultProgressBar(size int64) *progressbar.ProgressBar {
-	return progressbar.NewOptions64(
+func (c *Chunked) defaultProgressBar(size int64) {
+	c.tracker = progressbar.NewOptions64(
 		size,
 		progressbar.OptionSetWriter(os.Stderr),
 		progressbar.OptionEnableColorCodes(true),
@@ -52,25 +52,45 @@ func (c *Chunked) defaultProgressBar(size int64) *progressbar.ProgressBar {
 // Copies a file and displays progress to the tty.
 func (c *Chunked) Copy(dst io.Writer, src io.Reader) error {
 	// Create progress bar that will be displayed to the client.
-	stats, err := c.Source.Stat()
+	stats, err := src.(*os.File).Stat()
 	if err != nil {
 		return err
 	}
-	bar := c.defaultProgressBar(stats.Size())
 
-	// Upload a few chunks of 'src' at a time.
-	// TODO(bia): Should use a totalWritten accumulator as the loop condition.
-	for bar.State().CurrentBytes < float64(bar.State().Max) {
-		written, err := io.CopyN(dst, src, 6*1000*1000)
-		if err != nil && err != io.EOF {
+	c.defaultProgressBar(stats.Size())
+	c.beginCopy(dst, src)
+
+	return nil
+}
+
+// Begin copying a few chunks of 'src' at a time.
+// TODO(bia): Should use a totalWritten accumulator as the loop condition.
+func (c *Chunked) beginCopy(dst io.Writer, src io.Reader) error {
+	for c.tracker.State().CurrentBytes < float64(c.tracker.State().Max) {
+		if err := c.copyChunk(dst, src); err != nil {
 			return err
 		}
 
 		// Increment progress bar that will be rendered on the next refresh.
-		if err := bar.Add64(written); err != nil {
+		if err := c.incrementProgress(); err != nil {
 			return err
 		}
 	}
 
 	return nil
+}
+
+// Copies a file and displays progress to the tty.
+func (c *Chunked) copyChunk(dst io.Writer, src io.Reader) error {
+	written, err := io.CopyN(dst, src, 6*1000*1000)
+	if err != nil && err != io.EOF {
+		return err
+	}
+
+	c.written = written
+	return nil
+}
+
+func (c *Chunked) incrementProgress() error {
+	return c.tracker.Add64(c.written)
 }
